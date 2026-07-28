d8ab54f555a85f1aba51a7d736714a1dde9d5851
Víctor Kevin Pérez García <43688673+bicvaporub@users.noreply.github.com>
2026-07-27 19:55:20 -0600

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i)]++;
+            count[t.charAt(i)]--;
+        }
+
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
