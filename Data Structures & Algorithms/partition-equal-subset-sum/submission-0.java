75f8f6a25516febfdcb4f6740773bbc9ca4e9478
Víctor Kevin Pérez García <43688673+bicvaporub@users.noreply.github.com>
2026-08-19 22:37:27 -0600

    Add: partition-equal-subset-sum - submission-0

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-0.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public boolean canPartition(int[] nums) {
+        if (sum(nums) % 2 != 0) {
+            return false;
+        }
+
+        int target = sum(nums) / 2;
+        boolean[] dp = new boolean[target + 1];
+        boolean[] nextDp = new boolean[target + 1];
+
+        dp[0] = true;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j <= target; j++) {
+                if (j >= nums[i]) {
+                    nextDp[j] = dp[j] || dp[j - nums[i]];
+                } else {
+                    nextDp[j] = dp[j];
+                }
+            }
+            boolean[] temp = dp;
+            dp = nextDp;
+            nextDp = temp;
+        }
+
+        return dp[target];
+    }
+
+    private int sum(int[] nums) {
+        int total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+        return total;
+    }
+}
